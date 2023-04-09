f6dfb192cbc2086d4c473d58408ba9035b44d859
Maislinn <maislinn.helfer@gmail.com>
2023-04-08 20:21:31 -0700

    adding about page

---
+++ src/components/About.js
@@ -0,0 +1,18 @@
+function About() {
+    return (
+        <div className="jumbotron shadow-sm">
+            <h1 className="display-4"><i className="fas fa-user-circle"></i> About Me</h1>
+            <hr className="my-4" />
+            <p className='lead'>Maislinn recently made San Francisco her home for the fast-paced and forward-thinking lifestyle after graduating from Chapman University with a Bachelor of Science in Business and Economics. For the past few years, Maislinn has worked as an independent tech start-up consultant & researcher. Maislinn is an expert in market forecasting as well as the optimization and expansion of companies. Continuing her love for learning, Maislinn now attends the UC Berkeley Extension Full-Stack Developer program. Outside of her professional career, Maislinn volunteers her time and knowledge to multiple non-profits with a focus on education, accredited universities with a focus on decentralized technologies, and mentors students and entrepreneurs.</p>
+            <p className='lead'>
+                Maislinn's expertise has been in early-stage tech-start ups as a consultant and researcher. Now, Maislinn has decided to take her passion for technology and apply it to her career. She is currently attending the UC Berkeley Extension Full-Stack Developer program. She is excited to be a part of the tech industry and is looking for new opportunities to grow and expand her skill set.
+            </p>
+            <p className='lead'>
+                As a developer, Maislinn is detail-oriented, highly organized, and enjoys working collaboratively with others to achieve the best possible results. She is a quick learner and am always looking for new opportunities to grow and expand her knowledge base. Maislinn is passionate about technology and excited to be work in a more established tech role. 
+            </p>
+            <a className="btn btn-primary btn-lg" href="#" role="button">Hire Me!</a>
+        </div>
+    );
+}
+
+export default About;
